2094b0dee7665a2448b9724277c5f107c3c45bde
Saiyam Vaid <saiyamvaid1997@gmail.com>
2023-04-02 11:16:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [isVerified, setIsverified] = React.useState(false);
+
+  const handleSubscribe = () => {
+    if (isVerified == true) {
+      alert("You have successfully subscribed!");
+    } else {
+      alert("Please verify that you are a human!");
+    }
+  };
+
+  const onChange = (value) => {
+    // console.log(isVerified)
+    setIsverified(true);
+    console.log("Captcha value:", value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">React Recaptcha Demo - PentaCode</h1>
       </header>
+      <div className="App-intro">
+        <input type="text" placeholder="email@company.com" />
+
+        <div className="convert" onClick={handleSubscribe}>
+          Subscribe
+        </div>
+
+        <ReCAPTCHA
+        style={{justifyContent:"center",display:"flex",marginTop:"20px"}}
+          sitekey="6LetTUwlAAAAAP5OdQlrr3iVlSY4NHgHAU3_s-jN"
+          onChange={onChange}
+        />
+      </div>
     </div>
   );
 }
